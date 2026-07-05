edc0b5e7aaafd9e8da120da102f4ffbea065c8d1
Faa786 <muhammad.waseem593@gmail.com>
2026-07-05 18:00:35 +0500

    Add geocoding package import to location_service.dart

--- lib/core/services/location_service.dart
+++ lib/core/services/location_service.dart
@@ -1,4 +1,5 @@
 import 'package:geolocator/geolocator.dart';
+import 'package:geocoding/geocoding.dart' as geocoding;
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class LocationService {
